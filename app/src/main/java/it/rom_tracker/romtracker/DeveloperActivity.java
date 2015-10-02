8b894cd37e60bdce0eea59d5cc069b9deecfab4f
GabrielePC <gabryoryn53@gmail.com>
2015-10-02 15:26:45 +0200

    developer zone - add rom (1/3)

--- app/src/main/java/it/rom_tracker/romtracker/DeveloperActivity.java
+++ app/src/main/java/it/rom_tracker/romtracker/DeveloperActivity.java
@@ -2,38 +2,112 @@ package it.rom_tracker.romtracker;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class DeveloperActivity extends Activity {
+
     private Button addRom;
+    private TextView txtLinkRom;
+    private TextView txtNameRom;
+    private TextView txtChangelogRom;
+    private TextView txtVersionRom;
+
+    private String nameRom;
+    private String linkRom;
+    private String versionRom;
+    private String changelogRom;
+
+    private AlertDialog addRomDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer);
 
-        /////////////////////////////////////////////////
+        //////////////////////////////////////////////////////
 
+        //View dialog = getLayoutInflater().inflate(R.layout.dialog_add_rom, null);
         addRom = (Button) findViewById(R.id.btnAddRom);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        // add rom button cliecked
         addRom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LayoutInflater layoutInflater = getLayoutInflater();
+                final LayoutInflater layoutInflater = getLayoutInflater();
                 View dialog = layoutInflater.inflate(R.layout.dialog_add_rom, null);
-                new AlertDialog.Builder(DeveloperActivity.this)
+                ////////////////////////////////////////////////////////////////
+                txtLinkRom = (TextView) dialog.findViewById(R.id.txtLinkRom);
+                txtNameRom = (TextView) dialog.findViewById(R.id.txtNameRom);
+                txtChangelogRom = (TextView) dialog.findViewById(R.id.txtChangelogRom);
+                txtVersionRom = (TextView) dialog.findViewById(R.id.txtVersionRom);
+                ////////////////////////////////////////////////////////////////
+
+                // dialog for add new rom
+                addRomDialog = new AlertDialog.Builder(DeveloperActivity.this)
                         .setView(dialog)
-                        .create().show();
+                        .setPositiveButton(getString(R.string.add_rom), null)
+                        .create();
+                addRomDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(DialogInterface dialog) {
+                        Button addRom = addRomDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+                        addRom.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // checks for errors
+                                String error = checkErrorDialog();
+                                // no error on dialog
+                                if(!error.equals("")){
+                                    Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+                                }else{
+                                    // sending the data to parse
+                                    ParseObject newRom = new ParseObject("ROM");
+                                    newRom.put("name", nameRom);
+                                    newRom.put("version", versionRom);
+                                    newRom.put("link", linkRom);
+                                    if(changelogRom != null) {
+                                        newRom.put("changelog", changelogRom);
+                                    }
+                                    newRom.put("developer", ParseUser.getCurrentUser().getUsername());
+                                    newRom.saveInBackground(new SaveCallback() {
+                                        @Override
+                                        public void done(ParseException e) {
+                                            if(e == null){
+                                                // sent correctly
+                                                Toast.makeText(getApplicationContext(), getString(R.string.submitted_successfully), Toast.LENGTH_LONG).show();
+                                            }else{
+                                                // sending error
+                                                Toast.makeText(getApplicationContext(), getString(R.string.submitted_successfully), Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                                    //
+                                    addRomDialog.dismiss();
+                                }
+                            }
+                        });
+                    }
+                });
+                addRomDialog.show();
             }
         });
     }
@@ -43,6 +117,36 @@ public class DeveloperActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_developer, menu);
         return true;
+    }
+
+    private String checkErrorDialog(){
+        StringBuilder error = new StringBuilder();
+
+        String test = txtNameRom.getText().toString();
+        if(txtNameRom.getText().toString().equals("")){
+            error.append(getResources().getString(R.string.error_no_name_rom) + "\n");
+        }else{
+            nameRom = txtNameRom.getText().toString();
+        }
+
+        if(txtLinkRom.getText().toString().equals("")){
+            error.append(getResources().getString(R.string.error_no_link_rom) + "\n");
+        }else{
+            linkRom = txtLinkRom.getText().toString();
+        }
+
+        if(txtVersionRom.getText().toString().equals("")){
+            error.append(getResources().getString(R.string.error_no_version_rom));
+        }else{
+            versionRom = txtVersionRom.getText().toString();
+        }
+
+        // no mandatory
+        if(txtChangelogRom.getText().toString().equals("")){
+            changelogRom = txtChangelogRom.getText().toString();
+        }
+
+        return error.toString();
     }
 
     @Override
